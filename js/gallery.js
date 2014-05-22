62c4c48bdcbd76f91c1c8bbcedc74ed62cc48858
t13689hn <t13689hn@zmac114.sfc.keio.ac.jp>
2014-05-22 11:31:15 +0900

    2014/05/22
    コメントの追加

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,4 @@
-var photoListElement = document.querySelector("#photos");
+var photoListElement = document.querySelector("#photos");=//photolistElementの所在の設定
 
 var photoList = [
     {src: "img/200705.png", title: "7月"},
@@ -6,8 +6,14 @@ var photoList = [
 	  {src: "img/200902.png", title: "9月"},
 	  {src: "img/201008.png", title: "もう1回8月"},
     {src: "img/front.png", title: "前"},
-    {src:	"img/side.png", title: "横"}
-];
+    {src:	"img/side.png", title: "横"},
+    {src:	"img/hinamaturi-waku-2.jpg", title: "ひなまつり"}
+];//photolistの要素の掲載
+
+//var list[]でからっぽの配列ができる！！！！一番最後の要素にはカンマをつけない！！！！！
+//var numberOf Photo = photolist.length;
+//var firstPhoto = photolost(0); 0からはじめる
+//var lastphoto = photolist[numberOfphoto - 1]; 0からはじまっているから、−１して順番のずれをなおす
 
 var isReady = function(){
     return photoListElement != null &&
@@ -20,23 +26,24 @@ var renderPhoto = function(index){
     var elm = document.createElement("img");
     elm.setAttribute("src", photo.src);
     elm.setAttribute("title", photo.title);
-    return elm;
+    return elm;//elm
 };
 
 var showPhotos = function(){
     if(isReady()){
-        var index = 0;
-        while(index < photoList.length){
-            var elm = renderPhoto(index);
-            photoListElement.appendChild(elm);
-            index = index + 1;
+        var index = 0;//indexを0から開始する
+        while(index < photoList.length){//()の中がtrueである場合、{}の中を繰り返せ。indexがphotolistより大きくなったら処理が止まる
+
+            var elm = renderPhoto(index);//２４〜３０で選んだ写真（elm）をHTMLに表示する
+            photoListElement.appendChild(elm);//写真の表示
+            index = index + 1;//indexの値を１増加させる
         }
     }
 };
 
 var initApp = function(){
     var btn = document.querySelector("#startButton");
-    btn.addEventListener("click", showPhotos);
+    btn.addEventListener("click", showPhotos);//Buttonを作成し、clickという動作によってshowPhotosを起動させる=//
 };
 
-initApp();
+initApp();//関数の表示
